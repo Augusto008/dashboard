4aa84739bdf330b2e33b60731faeacb3a35aa383
Augusto008 <aquila.real0@gmail.com>
2023-11-13 07:57:54 -0300

    feat(`web.php`): add `Dashboard` route

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Views\CatalogController as Catalog;
+use App\Http\Controllers\Views\DashboardController as Dashboard;
 use App\Http\Controllers\Views\MainController as Main;
 
 
 Route::get('/', [Main::class, 'addLang']);
 Route::get('/{lang}', [Main::class, 'main']);
 Route::get('/{lang}/catalog', [Catalog::class, 'catalog']);
+Route::get('/{lang}/dashboard', [Dashboard::class, 'dashboard']);
