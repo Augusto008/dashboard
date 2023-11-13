e4200164cabdda44470aed8a48fe95f42a4c6d5e
Augusto008 <aquila.real0@gmail.com>
2023-11-13 16:55:13 -0300

    Feat(`FormularyController`): add `formulary` function

--- app/Http/Controllers/Views/FormularyController.php
+++ app/Http/Controllers/Views/FormularyController.php
@@ -7,5 +7,7 @@ use Illuminate\Http\Request;
 
 class FormularyController extends Controller
 {
-    //
+    public function formulary($lang) {
+        return view('welcome', ["title" => "formulary"]);
+    }
 }
