9f9a4fe24baa0df13b7dd667356e7a92ee2084ad
Augusto008 <aquila.real0@gmail.com>
2023-11-13 07:53:55 -0300

    feat(`web.php`): add `Main` routes

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Views\MainController as Main;
 
+
+Route::get('/', [Main::class, 'addLang']);
+Route::get('/{lang}', [Main::class, 'main']);
