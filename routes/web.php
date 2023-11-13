1d75079bd726601f7934dc3534a05b312d2145f2
Augusto008 <aquila.real0@gmail.com>
2023-11-13 07:58:43 -0300

    feat(`web.php`): add `Gallery` route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Views\CatalogController as Catalog;
 use App\Http\Controllers\Views\DashboardController as Dashboard;
 use App\Http\Controllers\Views\FormularyController as Formulary;
+use App\Http\Controllers\Views\GalleryController as Gallery;
 use App\Http\Controllers\Views\MainController as Main;
 
 
@@ -13,3 +14,4 @@ Route::get('/{lang}', [Main::class, 'main']);
 Route::get('/{lang}/catalog', [Catalog::class, 'catalog']);
 Route::get('/{lang}/dashboard', [Dashboard::class, 'dashboard']);
 Route::get('/{lang}/Formulary', [Formulary::class, 'formulary']);
+Route::get('/{lang}/gallery', [Gallery::class, 'gallery']);
