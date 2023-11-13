f30f05f69a1cd230049545f7b1099bee07f26d53
Augusto008 <aquila.real0@gmail.com>
2023-11-13 07:57:29 -0300

    feat(`web.php`): add `Catalog` route

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Views\CatalogController as Catalog;
 use App\Http\Controllers\Views\MainController as Main;
 
 
 Route::get('/', [Main::class, 'addLang']);
 Route::get('/{lang}', [Main::class, 'main']);
+Route::get('/{lang}/catalog', [Catalog::class, 'catalog']);
