99edecf2b0dc1e92f4d0f4f97ca7617aa621d6a6
Augusto008 <aquila.real0@gmail.com>
2023-11-13 16:52:10 -0300

    refactor(`MainController`): remove `addLang()` function

--- app/Http/Controllers/Views/MainController.php
+++ app/Http/Controllers/Views/MainController.php
@@ -10,12 +10,4 @@ class MainController extends Controller
     public function main($lang) {
         return view('welcome', ["title" => "main"]);
     }
-
-    public function addLang() {
-        $browserLang = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
-        $exploded = explode(',', $browserLang);
-        $lang = substr($exploded[0], 0, 2);
-        return redirect()->to("/$lang");
-    }
-
 }
