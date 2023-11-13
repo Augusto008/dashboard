7ff79a7b1ae52f9af08e68438ca9eae1e612b0a8
Augusto008 <aquila.real0@gmail.com>
2023-11-13 18:27:24 -0300

    Feat(`AddLangParameter.php`): add methods to validate language
    Validate language by checking the `lang` parameter.
    If `lang` parameter receive an invalid value, the value will be replaced by the browser language;
    If `lang` parameter receive an invalid value and the value of browser language is not in `$idioms` array, the value will be replaced by `en`;
    If `lang` parameter receive a route without language, the language will be added to the rout;
    And if the `lang` parameter receive an invalid value that cannot be used in a valid route,  the user will be redirected to the main page.

--- app/Http/Middleware/AddLangParameter.php
+++ app/Http/Middleware/AddLangParameter.php
@@ -16,6 +16,40 @@ class AddLangParameter
      */
     public function handle(Request $request, Closure $next): Response
     {
+        $lang = $request->segment(1);
+        $idioms = ['en', 'pt'];
+
+        // Se o lang não estiver definido ou não for válido, use o idioma do navegador
+        if (!$lang || !in_array($lang, $idioms)) {
+            $browserLanguages = $request->getLanguages();
+            $userLang = substr($browserLanguages[0], 0, 2);
+
+            // Use o idioma fornecido na URL se for válido, caso contrário, use o idioma do navegador
+            $defaultLang = "";
+            if(in_array($lang, $idioms)) {
+                $defaultLang = config('app.default_locale', $lang);
+            } else if(in_array($userLang, $idioms)) {
+                $defaultLang = config('app.default_locale', $userLang);
+            } else {
+                $defaultLang = config('app.default_locale', 'en');
+            };
+
+            // Verificar se o segmento 1 já é o idioma padrão para evitar um loop
+            if ($request->segment(1) !== $defaultLang) {
+                // Obter os segmentos da URL
+                $segments = $request->segments();
+
+                // Alterar o primeiro segmento apenas se não houver um idioma fornecido na URL
+                if (!$request->route('lang')) {
+                    $segments[0] = $defaultLang;
+                }
+
+                $correctedUrl = "/$defaultLang" . $request->getPathInfo();
+
+                return redirect($correctedUrl);
+            }
+        }
+
         return $next($request);
     }
 }
