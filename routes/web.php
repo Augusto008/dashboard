c8c6a8749f9fc71c7ba393bf7d095a1045aae4b3
Augusto008 <aquila.real0@gmail.com>
2023-11-13 07:58:16 -0300

    feat(`web.php`): add `Formulary` route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Views\CatalogController as Catalog;
 use App\Http\Controllers\Views\DashboardController as Dashboard;
+use App\Http\Controllers\Views\FormularyController as Formulary;
 use App\Http\Controllers\Views\MainController as Main;
 
 
@@ -11,3 +12,4 @@ Route::get('/', [Main::class, 'addLang']);
 Route::get('/{lang}', [Main::class, 'main']);
 Route::get('/{lang}/catalog', [Catalog::class, 'catalog']);
 Route::get('/{lang}/dashboard', [Dashboard::class, 'dashboard']);
+Route::get('/{lang}/Formulary', [Formulary::class, 'formulary']);
